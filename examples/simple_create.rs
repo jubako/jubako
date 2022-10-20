6e0c9464b982430b4ace9ec88b445606cc1fdcc3
Matthieu Gautier <mgautier@kymeria.fr>
2022-10-20 11:16:00 +0200

    Add small examples to create and read a simple container.

---
+++ examples/simple_create.rs
@@ -0,0 +1,109 @@
+use jubako as jbk;
+use std::error::Error;
+use std::rc::Rc;
+use typenum::{U31, U40, U63};
+
+// This is what will allow Jubako to differenciate your format from others.
+const VENDOR_ID: u32 = 0x01_02_03_04;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut content_pack = jbk::creator::ContentPackCreator::new(
+        "test.jbkc",
+        jbk::Id(1), // The pack id as referenced in the container
+        VENDOR_ID,
+        jbk::FreeData::<U40>::clone_from_slice(&[0x00; 40]), // Put whatever you what, this is for you
+        jbk::CompressionType::Zstd,                          // How to compress
+    );
+    content_pack.start()?;
+
+    let mut directory_pack = jbk::creator::DirectoryPackCreator::new(
+        "test.jbkd",
+        jbk::Id(0),
+        VENDOR_ID,
+        jbk::FreeData::<U31>::clone_from_slice(&[0x00; 31]),
+    );
+
+    // Entries have fixed sizes. We need to store variable length values in an extra store.
+    let value_store = directory_pack.create_key_store(jbk::creator::KeyStoreKind::Plain);
+
+    // Our entry kind will have two variants.
+    let entry_def = jbk::creator::Entry::new(vec![
+        jbk::creator::Variant::new(vec![
+            jbk::creator::Key::PString(0, Rc::clone(&value_store)), // One string, will be stored in value_store
+            jbk::creator::Key::new_int(),                           // A integer
+            jbk::creator::Key::ContentAddress,                      // A "pointer" to a content.
+        ]),
+        jbk::creator::Variant::new(vec![
+            jbk::creator::Key::PString(0, Rc::clone(&value_store)),
+            jbk::creator::Key::new_int(), //
+            jbk::creator::Key::new_int(), //
+        ]),
+    ]);
+
+    // The store for our entries.
+    let entry_store_id = directory_pack.create_entry_store(entry_def);
+    let entry_store = directory_pack.get_entry_store(entry_store_id);
+
+    // Now we have "configured" our container, let's add some content:
+    let content: Vec<u8> = "A super content prime quality for our test container".into();
+    let mut reader = jbk::creator::BufStream::new(content, jbk::End::None);
+    let content_id = content_pack.add_content(&mut reader)?;
+    entry_store.add_entry(
+        0, // Variant 0
+        vec![
+            jbk::creator::Value::Array("Super".into()),
+            jbk::creator::Value::Unsigned(50),
+            jbk::creator::Value::Content(jbk::creator::Content::from((
+                jbk::Id(1), // Pack id
+                content_id, // Content id in the pack
+            ))),
+        ],
+    );
+
+    entry_store.add_entry(
+        1, // Variant 1
+        vec![
+            jbk::creator::Value::Array("Mega".into()),
+            jbk::creator::Value::Unsigned(42),
+            jbk::creator::Value::Unsigned(5),
+        ],
+    );
+
+    entry_store.add_entry(
+        1, // Variant 1
+        vec![
+            jbk::creator::Value::Array("Hyper".into()),
+            jbk::creator::Value::Unsigned(45),
+            jbk::creator::Value::Unsigned(2),
+        ],
+    );
+
+    // One index to access our entries.
+    directory_pack.create_index(
+        "My own index",
+        jubako::ContentAddress::new(0.into(), 0.into()), // A pointer to a content which can be used to store whatever you what (nothing here)
+        0.into(),                                        // The index is not sorted
+        entry_store_id,
+        jubako::Count(3), // 3 entries
+        jubako::Idx(0),   // Offset 0
+    );
+
+    let directory_pack_info = directory_pack.finalize()?;
+    let content_pack_info = content_pack.finalize()?;
+    let mut manifest_creator = jbk::creator::ManifestPackCreator::new(
+        "test.jbkm",
+        VENDOR_ID,
+        jbk::FreeData::<U63>::clone_from_slice(&[0x00; 63]),
+    );
+
+    manifest_creator.add_pack(directory_pack_info);
+    manifest_creator.add_pack(content_pack_info);
+    manifest_creator.finalize()?;
+    // You have now 3 files : "test.jbkm", "test.jbkc" and "test.jbkd".
+
+    // Let's concat them in only one.
+    jbk::concat(&["test.jbkm", "test.jbkc", "test.jbkd"], "test.jbk")?;
+    // We have now 4 files. The 4th is "test.jbk" and it contains the 3 others.
+
+    Ok(())
+}
