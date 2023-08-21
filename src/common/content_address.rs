705821e9c7032d25235cd542fe0a9a9d8090bf2e
Matthieu Gautier <mgautier@kymeria.fr>
2023-08-21 09:47:09 +0200

    We don't need ContentAddress to be `Producable` or `Writable`

--- src/common/content_address.rs
+++ src/common/content_address.rs
@@ -15,23 +15,3 @@ impl ContentAddress {
         }
     }
 }
-
-impl Producable for ContentAddress {
-    type Output = Self;
-    fn produce(flux: &mut Flux) -> Result<Self> {
-        let pack_id = flux.read_u8()? as u16;
-        let content_id = flux.read_usized(ByteSize::U3)? as u32;
-        Ok(ContentAddress {
-            pack_id: pack_id.into(),
-            content_id: content_id.into(),
-        })
-    }
-}
-
-impl Writable for ContentAddress {
-    fn write(&self, stream: &mut dyn OutStream) -> IoResult<usize> {
-        let data: u32 = ((self.pack_id.into_u32() & 0x000000FF) << 24)
-            | (self.content_id.into_u32() & 0x00FFFFFF);
-        stream.write_u32(data)
-    }
-}
