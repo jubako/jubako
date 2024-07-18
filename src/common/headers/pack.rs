04a07a0aa775809345e1427a34c03e8e4a1ca6b6
Matthieu Gautier <mgautier@kymeria.fr>
2024-07-18 17:25:06 +0200

    Add flags in PackHeader.
    This way we are more prepared for future changes.

--- src/common/headers/pack.rs
+++ src/common/headers/pack.rs
@@ -26,6 +26,7 @@ pub struct PackHeader {
     pub major_version: u8,
     pub minor_version: u8,
     pub uuid: Uuid,
+    pub flags: u8,
     pub file_size: Size,
     pub check_info_pos: Offset,
 }
@@ -37,6 +38,7 @@ impl PackHeader {
             major_version: 0,
             minor_version: 1,
             uuid: Uuid::new_v4(),
+            flags: 0,
             app_vendor_id: pack_info.app_vendor_id,
             file_size: pack_info.file_size,
             check_info_pos: pack_info.check_info_pos,
@@ -56,7 +58,8 @@ impl Parsable for PackHeader {
         let major_version = parser.read_u8()?;
         let minor_version = parser.read_u8()?;
         let uuid = Uuid::parse(parser)?;
-        parser.skip(6)?;
+        let flags = parser.read_u8()?;
+        parser.skip(5)?;
         let file_size = Size::parse(parser)?;
         let check_info_pos = Offset::parse(parser)?;
         parser.skip(16)?;
@@ -66,6 +69,7 @@ impl Parsable for PackHeader {
             major_version,
             minor_version,
             uuid,
+            flags,
             file_size,
             check_info_pos,
         })
@@ -80,7 +84,8 @@ impl SizedParsable for PackHeader {
         + 1 // major
         + 1 // minor
         + Uuid::SIZE
-        + 6 // padding
+        + 1 // flags
+        + 5 // padding
         + Size::SIZE
         + Offset::SIZE
         + 16; // padding
@@ -94,7 +99,8 @@ impl Serializable for PackHeader {
         written += ser.write_u8(self.major_version)?;
         written += ser.write_u8(self.minor_version)?;
         written += self.uuid.serialize(ser)?;
-        written += ser.write_data(&[0_u8; 6])?;
+        written += ser.write_u8(self.flags)?;
+        written += ser.write_data(&[0_u8; 5])?;
         written += self.file_size.serialize(ser)?;
         written += self.check_info_pos.serialize(ser)?;
         written += ser.write_data(&[0_u8; 16])?;
@@ -117,6 +123,7 @@ impl serde::Serialize for PackHeader {
         };
         let mut obj = serializer.serialize_struct(name, 4)?;
         obj.serialize_field("uuid", &self.uuid)?;
+        obj.serialize_field("flags", &self.flags)?;
         obj.serialize_field("app_vendor_id", &self.app_vendor_id)?;
         obj.serialize_field("version", &(self.major_version, self.minor_version))?;
         obj.serialize_field("size", &self.file_size.into_u64())?;
@@ -137,7 +144,8 @@ mod tests {
             0x02, // minor_version
             0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,
             0x0e, 0x0f, // uuid
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // padding
+            0x00, // flags
+            0x00, 0x00, 0x00, 0x00, 0x00, // padding
             0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // file_size
             0xee, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // check_info_pos
             0x00, // No check
@@ -156,6 +164,7 @@ mod tests {
                     0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c,
                     0x0d, 0x0e, 0x0f
                 ]),
+                flags: 0,
                 file_size: Size::from(0xffff_u64),
                 check_info_pos: Offset::from(0xffee_u64),
             }
